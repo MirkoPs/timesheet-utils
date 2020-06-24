5b5b6c7a0bf5502e09ef88626267f28d519b874e
Mario Randazzo <randazzo.mario6996@gmail.com>
2020-06-24 17:55:01 +0200

    Added internal error and null data handling.

--- utils/validation.py
+++ utils/validation.py
@@ -1,13 +1,17 @@
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, InternalError
 from werkzeug.exceptions import Conflict, BadRequest
 
 
 def handle_invalid_data(func):
     def wrapper(*args, **kwargs):
         try:
+            if 'data' in kwargs.keys() and not kwargs['data']:
+                raise BadRequest("Body request not valid!")
             return func(*args, **kwargs)
         except IntegrityError as e:
             raise Conflict("Integrity error: {}".format(e.__cause__))
+        except InternalError as e:
+            raise BadRequest("Internal error: {}".format(e.__cause__))
         except ValueError as e:
             raise BadRequest("Validation error: {}".format(str(e)))
 
