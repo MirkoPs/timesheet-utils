f86ad3eaa0db79972237fe9c00c91fcaa09999ad
Mario Randazzo <randazzo.mario6996@gmail.com>
2020-06-26 16:26:57 +0200

    Added requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
-    description="A small example package",
+    description="to fill",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
@@ -19,4 +19,12 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        "requests~=2.23.0",
+        "werkzeug==0.16.1",
+        "flask~=1.1.2",
+        "flask_restplus",
+        "sqlalchemy~=1.3.17",
+        "py_eureka_client~=0.7.4"
+    ]
 )
