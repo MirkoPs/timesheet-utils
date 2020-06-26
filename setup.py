a82056b485250a83e03abbe78f5d8e0cfa3f094e
Mario Randazzo <randazzo.mario6996@gmail.com>
2020-06-26 16:10:04 +0200

    Refactoring package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="timesheet-utils",
+    name="timesheet-timesheet-utils",
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
