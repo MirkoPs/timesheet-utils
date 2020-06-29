401c552ca4160a0ad92bd6e786eb8c63fcad67a8
Mario Randazzo <randazzo.mario6996@gmail.com>
2020-06-29 10:40:37 +0200

    Fix service_discovery inititalization for testing

--- timesheet_utils/base.py
+++ timesheet_utils/base.py
@@ -9,17 +9,16 @@ def create_app(bp, eureka_path, app_name, init_schema_func, tenacity_wait=30):
     app = Flask(__name__)
     app.register_blueprint(bp)
 
-    @app.before_first_request
-    def _init():
-        if not app.config['TESTING']:
-            init_schema_func()
-
     @tenacity.retry(wait=tenacity.wait_fixed(tenacity_wait))
     def _init_service_discovery():
         eureka_client.init(eureka_server=eureka_path,
                            app_name=app_name,
                            instance_port=int(os.getenv('FLASK_RUN_PORT')))
 
-    _init_service_discovery()
+    @app.before_first_request
+    def _init():
+        if not app.config['TESTING']:
+            init_schema_func()
+            _init_service_discovery()
 
     return app
