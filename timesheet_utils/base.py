dccf7f6c9b88b1e0210b2c6205caef7f63929b7f
Mirko Pensato <mirko.pensato@cloudtec.it>
2021-05-14 14:42:59 +0200

    removed useless things

--- timesheet_utils/base.py
+++ timesheet_utils/base.py
@@ -1,8 +1,6 @@
 import os
 
-# import tenacity
 from flask import Flask
-# from py_eureka_client import eureka_client
 
 
 def get_db_path():
@@ -18,16 +16,9 @@ def get_db_path():
         )
     )
 
-# def get_eureka_path():
-#     return 'http://{}:{}/eureka/'.format(
-#         os.environ.get('EUREKA_HOST'),
-#         os.environ.get('EUREKA_PORT')
-#     )
-
 
 class BaseConfig(object):
     DATABASE_SERVER = get_db_path()
-    # EUREKA_SERVER = get_eureka_path()
     FLASK_RUN_PORT = int(os.environ.get('FLASK_RUN_PORT', '5000'))
 
 
@@ -37,15 +28,7 @@ def create_app(bp, app_name, init_schema_func, config=BaseConfig, tenacity_wait=
     app.register_blueprint(bp, url_prefix=prefix)
     app.config.from_object(config)
     app.url_map.strict_slashes = True
-    print("Database URI string ---> " + config.DATABASE_SERVER)
-    # print(config.EUREKA_SERVER)
-    # @tenacity.retry(wait=tenacity.wait_fixed(tenacity_wait))
-    # def _init_service_discovery():
-    #     eureka_client.init(eureka_server=config.EUREKA_SERVER,
-    #                        app_name=app_name,
-    #                        instance_port=config.FLASK_RUN_PORT)
     init_schema_func()
-    # _init_service_discovery()
     print("App initialized!!!")
     return app
 
